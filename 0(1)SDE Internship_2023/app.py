0b2fccc41ccbd654f44273820d6f9cec13cfeac4
Junedh Siddiqui <91174575+Junedhsiddiqui@users.noreply.github.com>
2023-08-30 23:29:58 +0530

    Add files via upload

---
+++ 0(1)SDE Internship_2023/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/url/task')
+def task_page():
+    return render_template('task.html')
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8000)
---
+++ 0(1)SDE Internship_2023/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dynamicDataDiv = document.getElementById('dynamic-data');
+    const dynamicContent = "This content is dynamically added using JavaScript.";
+    dynamicDataDiv.textContent = dynamicContent;
+});
